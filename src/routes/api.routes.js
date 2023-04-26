aaa1ceac2c511a5d8c8b2db5e05f4a8cbe5f94de
Carlos Kuhn <carlos.kuhn@usm.cl>
2023-04-26 13:54:27 -0400

    register

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -5,6 +5,7 @@ const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const util = require('util');
 const db = require('../db.js');
+const uuid = require('uuid');
 
 // Register
 router.post('/register', async(req, res) => {
@@ -20,12 +21,36 @@ router.post('/register', async(req, res) => {
     if(password != passwordConfirmation){
         return res.status(400).json({message: 'Las contraseñas no coinciden.'})
     }
+    try{
+        // Validar con base de datos
+        const existingUser = await db.query('SELECT * FROM users WHERE email = ?', [email]);
+        if(existingUser){
+            return res.status(409).json({message: 'Ya existe un usuario con este email.'})
+        }
+        const existingUsername = await db.query('SELECT * FROM users WHERE username = ?', [username]);
+        if (existingUsername.length > 0) {
+            return res.status(409).json({message: 'Ya existe un usuario con este nombre de usuario.' });
+        }
 
-    // Validar con base de datos
-    const existingUser = await user.findOne({email});
-    if(existingUser){
-        return res.status(409).json({message: 'Ya existe un usuario con este email.'})
+        // Cifrado de contraseña
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        // Generación ID de usuario
+        const userId = uuid.v4();
+
+        // Inserción en BD
+        await bd.query('INSERT INTO users SET ?', {userId, email, username, password: passwordHash});
+
+        // Token de autenticación
+        const token = jwt.sign({email, username}, 'charlesLakesEsUnPro', {expiresIn: '30d'});
+
+        res.json({message: 'Usuario registrado exitosamente.', token});
+        
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message: 'Error al registrar el usuario.'});
     }
+
 
 });
 
