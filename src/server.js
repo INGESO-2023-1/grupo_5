80202b5f65b9487819a277405661d5135dfae430
charleslakes <carloslagoscortes@hotmail.com>
2023-04-25 22:11:08 -0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('node:path');
+const app = express();
+
+// Configuraciones
+app.set('port',process.env.PORT || 5000)
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Rutas
+app.use('/api',require('./routes/api.routes.js'));
+
+// Archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// React
+app.get('*', (req,res) => res.sendFile(path.join(__dirname+'/public/index.html')))
+
+// Inicio server
+app.listen(app.get('port'),()=>{
+    console.log('Server on port 5000');
+});
