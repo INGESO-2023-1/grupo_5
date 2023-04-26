59009dd0187d414700bb78f422a92ceb5cea86e1
Carlos Lagos <carlos.lagosc@usm.cl>
2023-04-26 19:54:07 -0400

    connect register and backend

--- src/app/Login.js
+++ src/app/Login.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Swal from 'sweetalert2'
 import './login.css';
 
 function Login() {
+  
   const [isLogin, setIsLogin] = useState(true);
 
   const [loginFormData, setLoginFormData] = useState({
@@ -11,10 +13,10 @@ function Login() {
   });
 
   const [registerFormData, setRegisterFormData] = useState({
-    name: '',
+    username: '',
     email: '',
     password: '',
-    confirmPassword: ''
+    passwordConfirmation: ''
   });
 
   const handleLoginSubmit = (event) => {
@@ -31,8 +33,22 @@ function Login() {
 
   const handleRegisterSubmit = (event) => {
     event.preventDefault();
-    console.log(registerFormData);
-    // Aquí podrías enviar los datos del formulario de registro a tu servidor para procesar el registro
+    axios.post('/api/register',registerFormData)
+      .then((response) => {
+        console.log(response);
+        Swal.fire({
+          icon: 'success',
+          title: 'Felicidades',
+          text: response.data.message
+        });
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.response.data.message
+        })
+      });
   
   }
 
@@ -54,35 +70,27 @@ function Login() {
             <label>
               <input type="email" name="email" value={loginFormData.email} onChange={handleLoginChange} placeholder='Correo electrónico' className='my-input' />
             </label>
-            <br />
-            <label>
+            <label style={{marginBottom: "30px"}}>
               <input type="password" name="password" value={loginFormData.password} onChange={handleLoginChange} placeholder='Contraseña' className='my-input'/>
             </label>
-            <br />
-            <button type="submit" className='my-button'>Iniciar sesión</button>
-            <br />
+            <button type="submit" className='my-button' onClick={handleLoginSubmit}>Iniciar sesión</button>
             <button type="button" className='my-button' onClick={handleSwitchAuthMode}>Registrarse</button>
           </form>
         ) : (
           <form className='my-form'>
             <label>
-              <input type="text" name="name" value={registerFormData.name} onChange={handleRegisterChange} placeholder='Nombre de usuario' className='my-input'/>
+              <input type="text" name="username" value={registerFormData.name} onChange={handleRegisterChange} placeholder='Nombre de usuario' className='my-input'/>
             </label>
-            <br />
             <label>
               <input type="email" name="email" value={registerFormData.email} onChange={handleRegisterChange} placeholder='Correo electrónico' className='my-input' />
             </label>
-            <br />
             <label>
               <input type="password" name="password" value={registerFormData.password} onChange={handleRegisterChange} placeholder='Contraseña' className='my-input'/>
             </label>
-            <br />
-            <label>
-              <input type="password" name="confirmPassword" value={registerFormData.confirmPassword} onChange={handleRegisterChange} placeholder='Confirmar contraseña' className='my-input'/>
+            <label style={{marginBottom: "30px"}}>
+              <input type="password" name="passwordConfirmation" value={registerFormData.confirmPassword} onChange={handleRegisterChange} placeholder='Confirmar contraseña' className='my-input'/>
             </label>
-            <br />
-            <button type="submit" className='my-button'>Registrarse</button>
-            <br />
+            <button type="submit" className='my-button' onClick={handleRegisterSubmit}>Registrarse</button>
             <button type="button" className='my-button' onClick={handleSwitchAuthMode}>Iniciar sesión</button>
           </form>
         )}
